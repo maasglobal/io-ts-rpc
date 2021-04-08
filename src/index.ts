b63535200472eefe0de14dc059b2127a1947a3a3
Toni Ruottu <toni.ruottu@maas.global>
2021-04-08 14:42:28 +0300

    Fix default export names

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { Tunnel, tunnel } from './tunnel';
 export { HyperSchema } from './hyper-schema';
 export { Fetch, FetchLocation, FetchOptions } from './fetch';
-export { Endpoint, endpoint, fromHyperSchema } from './endpoint';
+export {
+  Endpoint,
+  endpoint,
+  fromHyperSchema as endpointFromHyperSchema,
+} from './endpoint';
